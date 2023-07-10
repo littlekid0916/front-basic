0cea542b3345a62e10539231fb034a83591252d6
heewon <littlekid916@naver.com>
2023-07-11 04:39:06 +0900

    react-basic

---
+++ react-basic/src/components/TestComponent/index.tsx
@@ -0,0 +1,43 @@
+//! 컴포넌트
+//! React의 기본 단위
+//! 화면을 독립적인 부분으로 나눠서 재사용성을 높이고
+//! 관리를 용이하게 해줌
+//! class 컴포넌트 / function 컴포넌트
+
+interface Props {
+  arg1: string;
+  arg2: number;
+}
+
+//! 컴포넌트 선언시 주의사항
+//! 컴포넌트명의 첫글자는 반드시 대문자이어야 함
+function TestComponent (props: Props) {
+
+  const itemList = ['첫번째', '두번째', '세번째', '네번째', '다섯번째'];
+  const flag = true;
+
+  //! return 위치에 화면에 출력할 HTML 작성
+  //! 컴포넌트 retrun 시 주의사항
+  //! 반드시 하나의 요소만 반환
+
+  //! return에 작성하는 HTML 내부에서 JavaScript를 사용하고 싶으면
+  //! {} 내부에 작성함
+  //! 그 {} 내부에서 다시 HTML을 반환하고 싶으면 () 내부에 작성
+  return (
+    <div>
+      <div>{props.arg1} {props.arg2}</div>
+      {/* 주석 */}
+      {itemList.map((item) => (<div>{item} 컴포넌트입니다.</div>))}
+      {
+        flag ? (<div>TRUE</div>) : (<div>FALSE</div>)
+      }
+    </div>
+  );
+};
+
+export default TestComponent;
+
+//! export, export default
+//! export : 한 페이지에서 여러개를 내보낼때 사용
+//! export default : 한 페이지에서 하나의 값을 기본값으로 내보낼때 사용
+export const testVariable = 10;
--- react-basic/src/views/JavaScriptBasic/index.tsx
+++ react-basic/src/views/JavaScriptBasic/index.tsx
@@ -100,6 +100,103 @@ function JavaScriptBasic() {
   result = number1 != number2;  // false
   result = number1 !== number2;  // true
 
+  // 조건문
+  // 조건문의 조건이 무조건 true, false 형태의 boolean 값이 오지 않아도 됨
+  // false : 0, 0.0, '', undefind, null
+  const string = '';
+  if (string) {
+
+  }
+
+  // 반복문
+  // for, while 모두 사용 가능
+  const exampleArray = ['apple', 'banana', 'carot'];
+  //! foreach 함수 : 배열.forEach(반환값이 없는 함수);
+  // for (String item: exampleArray) {
+
+  // }
+  exampleArray.forEach(function (item) {
+    console.log(item);
+  });
+
+  //! map 함수 : 배열.map(반환값이 있는 함수);
+  //!            배열을 반복하면서 함수 작업의 반환 값으로 새로운 배열을 생성하여 반환
+  const numbers = [1, 2, 3, 4, 5];
+  let resultList = numbers.map(function (item) {
+    const result = item * item;
+    return result;
+  });  // [1, 4, 9, 16, 25]
+
+  //! filter 함수 : 배열.filter(조건문을 가진 함수);
+  //!               배열을 반복하면서 함수 작성된 조건문에 부합하는 요소만 반환
+  resultList = numbers.filter(function (item) {
+    return item % 2 === 0;
+  });  // [2, 4]
+
+  //! 객체 생성
+  //! 1. JSON 형식을 이용하여 생성(바로 생성)
+  //! JSON : { 'key': 'value', 'key': 'value', ... }
+  let object1 = {
+    name: '홍길동',
+    age: 29,
+    address: '부산광역시'
+  };
+
+  //! 2. class를 이용하여 생성
+  class CHuman {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+  }
+  object1 = new CHuman('김철수', 30, '서울틀별시');
+
+  //! 3. interface를 이용하여 생성
+  interface IHuman {
+    name: string;
+    age: number;
+    address: string;
+  }
+  let object2: IHuman = {
+    name: '고길동',
+    age: 50,
+    address: '인천광역시'
+  }
+
+  object1.age;
+
+  //! 구조 분해 할당
+  // let name = object1.name;
+  // let age = object1.age;
+
+  let { name, age } = object1;  // { name: '홍길동', age: 30, address: '부산' }
+  console.log(name);
+  console.log(age);
+
+  let { address, ...other } = object1;
+  // address: '부산', other: { name: '홍길동', age: 30 }
+
+  let object3 = { telNumber: '010-1111-1111', email: 'email@email.com' };
+  let subObject = { ...object2, ...object3 };
+  // { name: '고길동', age: 50, adress: '인천광역시' }
+
+  //! 함수 
+  //! 함수 선언 방법
+  //! function 함수명 (매개변수: 타입, ...): 반환타입 {
+  //!  ...
+  //!  return 결과;
+  //! }
+
+  //! 화살표 함수
+  //! 함수를 변수처럼 사용하는 것
+  //! 선언 방법
+  //! const 함수명 = (매개변수, ...) => { ... return 결과값 };
+
   return (<></>);
 
 }
